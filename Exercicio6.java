26a68acd1c648532e5f172f6a8cb7b7116ebd193
Mateus Volpato Círico <mvcirico@furb.br>
2025-02-25 21:51:10 -0300

    Exerccios de java

---
+++ Exercicio6.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Exercicio6 {
+    public static void main(String[] args) {
+        Scanner tec = new Scanner(System.in);
+        System.out.println("Informe o peso do prato: ");
+        double pesoPrato = tec.nextDouble();
+        double valorTotal = pesoPrato * 25;
+        System.out.println("O valor total a pagar pelo prato é de: R$" + valorTotal);
+    }
+}
