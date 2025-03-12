baf7fa2cd133d2e9af5344a1c6ea7fea97d16327
Mateus Volpato Círico <mvcirico@furb.br>
2025-03-11 21:41:25 -0300

    Att

--- exemplo.java
+++ exemplo.java
@@ -3,6 +3,18 @@ import java.util.Scanner;
 
 public class exemplo {
     public static void main(String[] args) {
+        int numeroFuncionario, horasTrabalhadas;
+        double valorHora, salarioFuncionario;
+ 
+        Scanner s = new Scanner(System.in);
+        DecimalFormat formatador = new DecimalFormat("0.00");
+        numeroFuncionario = s.nextInt();
+        horasTrabalhadas = s.nextInt();
+        valorHora = s.nextDouble();
+        s.close();
+        salarioFuncionario = horasTrabalhadas * valorHora;
+        System.out.println("NUMBER = " + numeroFuncionario);
+        System.out.println("SALARY = U$ " + formatador.format(salarioFuncionario));
         
     }
 }
