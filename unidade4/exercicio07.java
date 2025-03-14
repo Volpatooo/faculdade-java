66b830cf9deb21c71e9969a0249950f0a8b30ca2
Volpatooo <teteuvolpato@gmail.com>
2025-03-14 09:19:30 -0300

    Finalizados exercicios unidade04

---
+++ unidade4/exercicio07.java
@@ -0,0 +1,22 @@
+package unidade4;
+
+import java.util.Scanner;
+
+public class exercicio07 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Informe uma gramatura: ");
+        int gramaturaForncecida = s.nextInt();
+        double precoFixo = 0.45;
+        double valorTotal = 0;
+
+        if (gramaturaForncecida <= 50) {
+            System.out.println("Custo do selo e de: " + precoFixo);
+        } else {
+            int gramaturaAdicional = gramaturaForncecida - 50;
+            double precoAdicional = (gramaturaAdicional / 20) + 1;
+            valorTotal = precoFixo + 0.45 * precoAdicional;
+            System.out.println("Custo do selo e de: " + valorTotal);
+        }
+    }
+}
