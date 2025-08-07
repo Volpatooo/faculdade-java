e2bfa12ac64c732e8e3ceda98a01ad18676656fc
Mateus Volpato Círico <mvcirico@furb.br>
2025-08-07 19:51:30 -0300

    Unidade 1 POO

--- _2semestre/unidade1/pessoa.java
+++ _2semestre/unidade1/pessoa.java
@@ -1,10 +1,14 @@
 package _2semestre.unidade1;
 
-public class pessoa {
+public class Pessoa {
     double peso;
     double altura;
 
     double calcularImc() {
         return peso / (altura * altura);
+    }
+
+    void comer(double quantidade) { // Método vazio sem retorno
+        peso = peso + (quantidade / 1000);
     }
 }
